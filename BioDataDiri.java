df314f1aa341d545b92c76b1fb251e578e69c6e1
Nurul Hikmawati <146693374+l0vnurul@users.noreply.github.com>
2023-10-10 13:03:29 +0700

    day 2

---
+++ BioDataDiri.java
@@ -0,0 +1,21 @@
+
+public class BioDataDiri {
+    public static void main(String[] args) {
+        // membuat variabel
+        String nama, alamat;
+        int usia;
+        double tinggi;
+        //mengisi variabel
+        nama = "Nurul Hikmawati";
+        alamat = "Mamuju, Kecamatan Sampaga, Desa Bunde";
+        usia = 18;
+        tinggi = 149;
+        // mencetak kelayar isi variabel
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Usia: " + usia + "tahun");
+        System.out.println("tinggi: " + tinggi + "cm");
+        
+    }
+    
+}
