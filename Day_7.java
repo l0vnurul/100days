1d7b1623c668c2bfeffa5b9b4389b81676c0c734
l0vnurul <nurulhkmawati01@gmail.com>
2023-10-15 08:48:07 +0700

    day 7

---
+++ Day_7.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+
+public class Day_7 {
+   public static void main(String[]args) {
+       int angka5;
+       int angka4;
+       int angka7;
+       int angka3;
+       int hasil;
+       Scanner keyboard = new Scanner(System.in);
+       System.out.println("Input angka-5: ");
+       angka5 = keyboard.nextInt();
+       System.out.println("Input angka-4: ");
+       angka4 = keyboard.nextInt();
+       System.out.println("Input angka-7: ");
+       angka7 = keyboard.nextInt();
+       System.out.println("Input angka-3: ");
+       angka3 = keyboard.nextInt();
+       
+       // pengurangan
+       hasil = angka5 - angka4;
+       System.out.println("Input angka-5: ");
+       angka5 = keyboard.nextInt ();
+       System.out.println("Input angka-4: ");
+       angka4 = keyboard.nextInt();
+       hasil = angka7 - angka3;
+       System.out.println("Input angka-7: ");
+       angka7 = keyboard.nextInt();
+       System.out.println("Input angka-3: ");
+       angka3 = keyboard.nextInt();
+       
+       // perkalian
+       hasil = angka5 * angka4;
+       System.out.println("Hasil = " + hasil);
+       System.out.println("Input angka-5: ");
+       angka5 = keyboard.nextInt();
+       System.out.println("Input angka-4: ");
+       angka4 = keyboard.nextInt();
+       
+       hasil = angka7 * angka3;
+       System.out.println("Hasil = " + hasil);
+       System.out.println("Input angka-7: ");
+       angka7 = keyboard.nextInt();
+       System.out.println(" Input angka-3: ");
+       angka3 = keyboard.nextInt();
+      
+       
+               
+   } 
+}
