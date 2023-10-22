0307feb04d8111df7823ed90a0a607d04fa343d8
l0vnurul <nurulhkmawati01@gmail.com>
2023-10-22 08:23:26 +0700

    Operator Penugasan

---
+++ Day_13.java
@@ -0,0 +1,30 @@
+
+public class Day_13 {
+    public static void main(String[]args) {
+        int nilaiE = 18;
+        int nilaiD = 4;
+        boolean hasil;
+        
+        // Apakah D lebih besar dari E?
+        hasil = nilaiD > nilaiE; 
+        System.out.println(hasil);
+        
+        
+        // Apakah D lebih kecil dari E?
+        hasil = nilaiD < nilaiE;
+        System.out.println(hasil);
+        
+        // Apakah A lebih besar sama dengan e?
+        hasil = nilaiD >= nilaiE;
+        System.out.println(hasil);
+        
+        // Apakah D lebih kecil sama dengan E?
+        hasil = nilaiD <= nilaiE;
+        System.out.println(hasil);
+        
+        // Apakah nilai D tidak sama dengan E?
+        hasil = nilaiD != nilaiE;
+        System.out.println(hasil);
+        
+    }
+}
