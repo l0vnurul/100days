76a35901fb44bab84bfc27025168c2e349fa91e9
l0vnurul <nurulhkmawati01@gmail.com>
2023-11-01 07:48:53 +0700

    Day 23 biodata singkat menggunakan Scanner

---
+++ Day23.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Day23 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String nama_,alamat_,nim_,kelas$;
+        double TB_;
+        float BB_;
+        
+        
+        System.out.print("Nama: ");
+        nama_= sc.nextLine();
+        System.out.print("Alamat: ");
+        alamat_= sc.nextLine();
+        System.out.print("Nim: ");
+        nim_= sc.nextLine();
+        System.out.print("Kelas: ");
+        kelas$= sc.nextLine();
+        System.out.print("TB: ");
+        TB_= sc.nextDouble();
+        System.out.print("BB: ");
+        BB_= sc.nextFloat();
+        
+        
+                
+    }
+    
+}
