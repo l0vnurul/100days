e12f90a697dd7b2cd7b5c342bab141c7dda254d1
l0vnurul <nurulhkmawati01@gmail.com>
2023-11-13 08:58:32 +0700

    Day35 pemograman menggunakan Array Multi Dimensi

---
+++ Day35.java
@@ -0,0 +1,10 @@
+public class Day35 {
+    public static void main(String[] args) {
+        String[][] kontak = {{"Nurul","08134"}, {"imma","08136"}, {"Wati","08898"}};
+        for(int x=0; x < kontak.length; x++){
+            System.out.println("Nama: " + kontak[x][0]);
+            System.out.println("Nomor " + kontak [x][1]);;
+            System.out.println("-----------------------------------");
+        }
+    }
+}
