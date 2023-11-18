de32cde7427e5729eea66cfd0d6dd7fd1e7879c2
l0vnurul <nurulhkmawati01@gmail.com>
2023-11-19 05:51:31 +0700

    day42 operator logika

---
+++ Day42.java
@@ -0,0 +1,15 @@
+public class Day42 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c;
+        
+        c = a&&b;
+        System.out.println("true && false = " + c);
+        
+        c = a||b;
+        System.out.println("negasi. ! true = " + a);
+        }
+        
+    }
+
