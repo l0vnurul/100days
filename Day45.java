b2da8ac529fcb9a98a385ce28c9456e0e82ec1de
l0vnurul <nurulhkmawati01@gmail.com>
2023-11-22 04:35:31 +0700

    Day45 Perulangan While

---
+++ Day45.java
@@ -0,0 +1,10 @@
+public class Day45 {
+    public static void main(String[] args) {
+        int bilangan =1;
+        while(bilangan <=20){
+            System.out.println("Perulangan ke : " + (bilangan + 5));
+            bilangan++;
+        }
+    }
+    
+}
