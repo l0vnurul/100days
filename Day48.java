fc45c7a5f2553ffbfb3502899f9119bd8e4a8582
l0vnurul <nurulhkmawati01@gmail.com>
2023-11-25 04:52:06 +0700

    day48 perulangan for

---
+++ Day48.java
@@ -0,0 +1,10 @@
+public class Day48 {
+    public static void main(String[] args) {
+         for(int i= 100; i>=1; i--){
+            if(i % 2 == 0){
+            System.out.println("angka " + i);
+        }
+        }
+    }
+    
+}
