5579ac5bc028fd9c33fbc5714f81fc48f77caf52
l0vnurul <nurulhkmawati01@gmail.com>
2023-11-26 03:45:56 +0700

    Day49 Perulangan For

---
+++ Day49.java
@@ -0,0 +1,15 @@
+public class Day49 {
+    public static void main(String[] args) {
+       // MencetakBilanganGanjil
+       
+       for(int i = 1; i <= 100; i += 2){
+        System.out.print(i + " ");
+            
+                        
+        }
+       
+        
+        
+    }
+    
+}
