3c6bbf1d39201b8b80825ffb860768b2b21f0e01
l0vnurul <nurulhkmawati01@gmail.com>
2023-11-27 05:40:52 +0700

    day50 Perulangan ForEach

---
+++ Day50.java
@@ -0,0 +1,13 @@
+public class Day50 {
+    public static void main(String[] args) {
+        // prulanganForEach
+        
+        // membuat array
+        int angka[] = {4,2,6,8};
+        // menggunakan perulangan for each untu menampilkan angka
+        for( int x : angka ){
+            System.out.print(x + " ");
+        }
+    }
+    
+}
