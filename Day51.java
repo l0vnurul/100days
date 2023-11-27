291e68765c8b27f4bff9638859b74122382f4b9e
l0vnurul <nurulhkmawati01@gmail.com>
2023-11-28 05:05:53 +0700

    day51 Mencetak isi Array dengan kelas Arrays

---
+++ Day51.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+public class Day51 {
+    public static void main(String[] args) {
+        
+      int[] nilai = {40,25,32,46,55};
+      
+        System.out.print(Arrays.toString(nilai));
+    }
+    
+}
