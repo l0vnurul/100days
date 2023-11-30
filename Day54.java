d40b69047dcd0979a30caff85f0d722855efb01a
l0vnurul <nurulhkmawati01@gmail.com>
2023-12-01 03:42:28 +0700

    Day 54 perulangan while

---
+++ Day54.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Day54 {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int angka;
+        angka=2;
+        while (angka<=10) {
+            System.out.println(angka);
+            angka+=2;
+        }
+    }
+    
+}
