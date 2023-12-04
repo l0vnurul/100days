0ebf018bbdd42720f475d133d3f66ef5cac2500d
l0vnurul <nurulhkmawati01@gmail.com>
2023-12-05 05:10:15 +0700

    day 58 Array

---
+++ Day58.java
@@ -0,0 +1,8 @@
+public class Day58 {
+    public static void main(String[] args) {
+        // mengambil data dari array
+        String[] nama = {"Wahyu", "Desi", "Putra", "Kadek", "Lia" };
+        System.out.println(nama[4]);
+    }
+    
+}
