587707763848d6dd8431c1d0ed1602229c9d0c24
l0vnurul <nurulhkmawati01@gmail.com>
2023-12-06 01:01:25 +0700

    Array

---
+++ Day59.java
@@ -0,0 +1,12 @@
+public class Day59 {
+    public static void main(String[] args) {
+        String[][] ukuran = {{"Baju","M"}, {"Celana","L"},{"Jilbab","M"},{"Topi","S"}};
+        
+        for(int x=0; x < ukuran.length; x++){
+        System.out.println("Benda: " + ukuran[x][0]);
+            System.out.println("Ukuran: " + ukuran[x][1]);
+            System.out.println("----------------------------");
+    }
+    }
+    
+}
