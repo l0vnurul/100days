8de113692f2bb88611f21e185e06c87dc5769f63
l0vnurul <nurulhkmawati01@gmail.com>
2023-12-07 04:53:52 +0700

    Day 60 array

---
+++ Day60.java
@@ -0,0 +1,14 @@
+public class Day60 {
+    public static void main(String[] args) {
+         // membuat program array
+         // menginput variabel
+         int[]a = new int[3];
+         // menginput indeks
+         a[0] = 7;
+         a[1] = 8;
+         a[2] = 2;
+         System.out.println(a[2]);
+         
+    }
+   
+}
