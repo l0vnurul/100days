e57f77e3f8a09b2ed11bc95e95368a74f529f9da
l0vnurul <nurulhkmawati01@gmail.com>
2023-12-16 02:59:57 +0700

    Array

---
+++ Day69.java
@@ -0,0 +1,12 @@
+public class Day69 {
+    public static void main(String[] args) {
+        // menampilkan angka array
+        // menginput variabel
+          double[] array = {20.0 ,2.5,8.6,66.2,43.2};
+        // mencetak dengan menggunakan perulangan for
+        for(double b : array){
+            System.out.println(b);
+        }
+    }
+    
+}
